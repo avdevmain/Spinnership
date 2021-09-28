67e9a9ad70473f2f1e108ca4138cf7eefa8a8aa1
avdevmain <antonvolegov@mail.ru>
2021-09-28 18:22:21 +0300

    Cleanup, basic weapon look

--- Spinnership/Assets/Scripts/CameraFollow.cs
+++ Spinnership/Assets/Scripts/CameraFollow.cs
@@ -16,7 +16,5 @@ public class CameraFollow : MonoBehaviour
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
-
-        //transform.LookAt(target);
     }
 }
