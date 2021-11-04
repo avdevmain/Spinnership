5149a8960086d79e6230141b93ba72b0629bd17a
avdevmain <antonvolegov@mail.ru>
2021-11-04 14:23:55 +0300

    Camera limited with level

--- Spinnership/Assets/Scripts/CameraFollow.cs
+++ Spinnership/Assets/Scripts/CameraFollow.cs
@@ -11,10 +11,21 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed = 12.5f;
     public Vector3 offset;
 
+
+
     void FixedUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        
+        Debug.Log(smoothedPosition.y);
+        if ((smoothedPosition.x < -13.5f) || (smoothedPosition.x > 13.5f)) //Level border values
+            smoothedPosition.x = transform.position.x;
+
+        if ((smoothedPosition.y < -6f)||(smoothedPosition.y > 18.5f))
+            smoothedPosition.y = transform.position.y;
+
+        
         transform.position = smoothedPosition;
     }
 }
